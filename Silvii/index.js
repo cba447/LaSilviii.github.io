7e5bc1b53521a8ce9d5f1b5579e49fb586e7583f
cba447 <95392388+cba447@users.noreply.github.com>
2022-05-02 20:29:22 -0500

    Add files via upload

---
+++ Silvii/index.js
@@ -0,0 +1,9 @@
+alert("Hola, soy una computadora que resuelve dudas")
+alert("Listo?")
+alert("...")
+a = confirm("¿Queres saber a quien se coge Seba?")
+if (a = true) {
+    alert("A la Silviiiiiiiii")
+}else{
+    alert("A la Silviiiiiiiii")
+}
